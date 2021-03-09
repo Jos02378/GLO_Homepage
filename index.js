888b802dab253500a3a7478df362fbc6a08ea00d
Ernest Salim <salim.ernest17@gmail.com>
2021-03-09 16:34:36 +0700

    Update banyak

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const navTrigger = document.querySelector('.navigation_trigger');
 const navPanel = document.querySelector('.navigation_panel');
 const navBackground = document.querySelector('.navigation_background');
 const navBox = document.querySelector('.navigation_box');
+const wrapper = document.querySelector('.wrapper');
 
 navTrigger.addEventListener('click', () => {
     navPanel.style.opacity = 1;
     navPanel.style['pointer-events'] = 'unset';
+    wrapper.style.height = '100vh';
     setTimeout(() => {
         navBox.style.transform = 'translateY(0)';
     }, 300);
@@ -16,5 +18,6 @@ navBackground.addEventListener('click', () => {
     setTimeout(() => {
         navPanel.style.opacity = 0;
         navPanel.style['pointer-events'] = 'none';
+        wrapper.style.height = 'unset';
     }, 500);
 });
